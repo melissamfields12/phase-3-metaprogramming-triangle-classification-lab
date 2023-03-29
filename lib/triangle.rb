c2588882da4dda223b6000ae148bb4ed48c1ba8a
Melissa Fields <melissamfields12@gmail.com>
2023-03-29 16:39:56 -0700

    commit tree 0331b678f5170174dc92de47374c55cb840069ef

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+
+attr_accessor :sidea, :sideb, :sidec
+
+def initialize(sidea, sideb, sidec)
+  @sidea = sidea
+  @sideb = sideb
+  @sidec = sidec
+end
+
+
+def kind
+  if self.sidea > 0 == self.sideb and self.sidea
+    :equilateral
+  elsif self.sidea > 0 == self.sideb || self.sidec
+    :isosceles
+  elsif self.sidea > 0 != self.sideb || self.sidec && self.sideb > 0 != self.sidec > 0
+    :scalene
+  else
+    raise TriangleError
+  end
+end
+end
+
+class TriangleError < StandardError
+  def message
+    "This is not a valid triangle!"
+end
+
 end
